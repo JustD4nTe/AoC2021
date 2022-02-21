67a98a8e307bcad9fa7a5e84a0b4b8989ceba6cf
Dominik Tyc <Domikxf@gmail.com>
2022-02-21 18:13:53 +0100

    Day6

---
+++ Day6/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+
+namespace AoC2021.Day6
+{
+    internal class Solution
+    {
+        private readonly List<long> _days;
+        public Solution()
+        {
+            var temp = Helper.ReadFile("Day6")
+                             .Split(",")
+                             .Select(x => int.Parse(x));
+
+            _days = new List<long>();
+
+            for(var i = 0; i <= 8; i++)
+                _days.Add((long)temp.Count(x => x == i));
+        }
+        public long PartOne()
+        {
+            for(var i = 0; i < 80; i++)
+            {
+                _days[7] += _days[0];
+                _days.Add(_days[0]);
+                _days.RemoveAt(0);
+            }
+
+            return _days.Sum();
+        }
+
+        public long PartTwo()
+        {
+            for (var i = 0; i < 256; i++)
+            {
+                _days[7] += _days[0];
+                _days.Add(_days[0]);
+                _days.RemoveAt(0);
+            }
+
+            return _days.Sum();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using AoC2021.Day5;
+﻿using AoC2021.Day6;
 
 Console.WriteLine("Part One: " + new Solution().PartOne());
 Console.WriteLine("Part Two: " + new Solution().PartTwo());
